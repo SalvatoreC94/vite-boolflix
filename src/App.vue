d459b0092f9703feb9de96e3a8f4f43b54a975ce
SalvatoreC94 <salvatorecozzolino.1594@gmail.com>
2024-09-19 10:59:04 +0200

    fixato milestone 2 (separato serie e movie)

--- src/App.vue
+++ src/App.vue
@@ -6,21 +6,33 @@ export default {
     return {
       apiKey: 'e5b9f07cb974b5607241361f444c4724',
       searchtext: '',
-      movies: []
+      searchType: 'movie', // Valore di default per cercare film
+      movies: [],
+      series: []
     }
   },
   methods: {
-    searchMovies() {
+    searchContent() {
+      const url =
+        this.searchType === 'movie'
+          ? 'https://api.themoviedb.org/3/search/movie'
+          : 'https://api.themoviedb.org/3/search/tv';
+
       axios
-        .get('https://api.themoviedb.org/3/search/movie', {
+        .get(url, {
           params: {
             api_key: this.apiKey,
             query: this.searchtext
           }
         })
-        
-        .then(response => {
-          this.movies = response.data.results;
+        .then(res => {
+          if (this.searchType === 'movie') {
+            this.movies = res.data.results;
+            this.series = []; // Reset delle serie quando si cercano i film
+          } else {
+            this.series = res.data.results;
+            this.movies = []; // Reset dei film quando si cercano le serie
+          }
         })
         .catch(error => {
           console.error("Errore nell'API:", error);
@@ -31,65 +43,76 @@ export default {
 </script>
 
 <template>
-<div class="container mt-5">
-    <form @submit.prevent="searchMovies" class="d-flex mb-4">
+  <div class="container mt-5">
+    <form @submit.prevent="searchContent" class="d-flex mb-4">
       <input
         v-model="searchtext"
         type="text"
         class="form-control me-2"
-        placeholder="Cerca film o serie"/>
+        placeholder="Cerca film o serie TV"/>
+      <select v-model="searchType" class="form-select me-2">
+        <option value="movie">Film</option>
+        <option value="tv">Serie TV</option>
+      </select>
       <button type="submit" class="btn btn-primary">Cerca</button>
     </form>
 
-    <!-- Movies List -->
-
     <div class="list-group">
-  <div
-    v-for="(movie, index) in movies" :key="index"
-    class="list-group-item list-group-item-action">
-    
-    <h5 class="mb-1">{{ movie.title || movie.name}}</h5>
-    
-    <p class="mb-1"> 
-      <strong>Titolo Originale:</strong> {{ movie.original_title || movie.original_name}}
-     
-    </p>
-    
-    <p class="mb-1">
-     
-      <template v-if=" movie.original_language == 'en'">
-  <strong>Lingua:</strong> <img src="/img/flags/en.jpg" alt="">
-</template>
-<template v-if=" movie.original_language == 'it'">
-  <strong>Lingua:</strong> <img src="/img/flags/it.jpg" alt="">
-</template>
-<template v-if=" movie.original_language == 'ja'">
-  <strong>Lingua:</strong> <img src="/img/flags/jp.png" alt="">
-</template>
-<template v-else>
-  <p class="mb-1">
-      <strong>Lingua:</strong> {{ movie.original_language || 'N/A' }}
-    </p>
-</template>
-   
-    </p>
-    
-    <small><strong>Voto:</strong> {{ movie.vote_average}}</small>
-    
+      <div
+        v-for="(movie, index) in movies" :key="index"
+        class="list-group-item list-group-item-action">
+        <h5 class="mb-1">{{ movie.title }}</h5>
+        <p class="mb-1"> 
+          <strong>Titolo Originale:</strong> {{ movie.original_title }}
+        </p>
+        <p class="mb-1">
+          <template v-if="movie.original_language === 'en'">
+            <strong>Lingua:</strong> <img src="/img/flags/en.jpg" alt="">
+          </template>
+          <template v-else-if="movie.original_language === 'it'">
+            <strong>Lingua:</strong> <img src="/img/flags/it.jpg" alt="">
+          </template>
+          <template v-else-if="movie.original_language === 'ja'">
+            <strong>Lingua:</strong> <img src="/img/flags/jp.png" alt="">
+          </template>
+          <template v-else>
+            <strong>Lingua:</strong> {{ movie.original_language }}
+          </template>
+        </p>
+        <small><strong>Voto:</strong> {{ movie.vote_average }}</small>
+      </div>
+
+      <div
+        v-for="(serie, index) in series" :key="index"
+        class="list-group-item list-group-item-action">
+        <h5 class="mb-1">{{ serie.name }}</h5>
+        <p class="mb-1">
+          <strong>Titolo Originale:</strong> {{ serie.original_name }}
+        </p>
+        <p class="mb-1">
+          <template v-if="serie.original_language === 'en'">
+            <strong>Lingua:</strong> <img src="/img/flags/en.jpg" alt="">
+          </template>
+          <template v-else-if="serie.original_language === 'it'">
+            <strong>Lingua:</strong> <img src="/img/flags/it.jpg" alt="">
+          </template>
+          <template v-else-if="serie.original_language === 'ja'">
+            <strong>Lingua:</strong> <img src="/img/flags/jp.png" alt="">
+          </template>
+          <template v-else>
+            <strong>Lingua:</strong> {{ serie.original_language }}
+          </template>
+        </p>
+        <small><strong>Voto:</strong> {{ serie.vote_average }}</small>
+      </div>
+    </div>
   </div>
-</div>
-    
-</div>
-    
 </template>
-
-
 
 <style lang="scss">
-@use 'assets/scss/main' as * ;
+@use 'assets/scss/main' as *;
 /*  Import all of Bootstrap's CSS */
 @import "bootstrap/scss/bootstrap";
-
 
 .container {
   max-width: 800px;
@@ -100,8 +123,8 @@ export default {
   border: 1px solid #ddd;
   margin-bottom: 10px;
 }
-img{
+
+img {
   max-width: 40px;
 }
-
 </style>
